5dfdf9c630e26c384c46ac715833ce91b477b874
Darren <1240101543@qq.com>
2020-07-21 21:55:27 +0800

    从Frame类继承，并且重写paint方法，画出一个黑方块

---
+++ src/main/java/com/darren/center/Main.java
@@ -0,0 +1,10 @@
+package com.darren.center;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        TankFrame tf = new TankFrame();
+    }
+
+}
---
+++ src/main/java/com/darren/center/TankFrame.java
@@ -0,0 +1,40 @@
+package com.darren.center;
+
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * 写一个新窗口
+ */
+public class TankFrame extends Frame {
+
+    public TankFrame(){
+            //设置大小
+            setSize(800, 600);
+            //不可改变大小
+            setResizable(false);
+            //设置标题
+            setTitle("tank war");
+            //设置可见
+            setVisible(true);
+            //关闭事件监听
+            addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+    }
+
+    /**
+     * 窗口重新绘制的时候自动调用
+     * @param g
+     */
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint");
+        g.fillRect(200,200,50,50);
+    }
+}
