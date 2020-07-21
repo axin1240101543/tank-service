6b4a36f5a784b1a997ad7e5755675d5df956756a
Darren <1240101543@qq.com>
2020-07-21 21:59:35 +0800

    坦克坐标定为变量，在paint中改变值，paint被调用后就可以动起来

--- src/main/java/com/darren/center/TankFrame.java
+++ src/main/java/com/darren/center/TankFrame.java
@@ -10,6 +10,8 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
 
+    int x = 200, y = 200;
+
     public TankFrame(){
             //设置大小
             setSize(800, 600);
@@ -35,6 +37,8 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         System.out.println("paint");
-        g.fillRect(200,200,50,50);
+        g.fillRect(x, y,50,50);
+        x += 10;
+        y += 10;
     }
 }
